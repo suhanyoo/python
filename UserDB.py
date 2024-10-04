f0bcf50970e0a083455de87611b9336fb08b4a0f
suhanyoo <81296501+suhanyoo@users.noreply.github.com>
2024-10-04 12:32:49 +0900

    회원 정보 조회 및 회원가입
    아이디 찾기 , 비밀번호 찾기 회원가입은 아직 만들다가 말았음

--- UserDB.py
+++ UserDB.py
@@ -1,7 +1,63 @@
 #유저 정보 가져오기 DB 연동 한 코드 
 import mysql.connector
+import random
 
-def UserLogin(userid):
+
+# UserLogin Function 은 사용자가 로그인 가능 여부 
+def UserLogin(userid,userpw):
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user ="root",
+        password ="ahemsdl00*",
+        database ="BingoUserDB"
+    )
+    cursor = connection.cursor(dictionary=True)
+
+    # query 변수에 담아서 SQL 인잭션 방지 하여서 파라미터 바인딩 
+    query = "select * from UserInformation where UserID = %s and UserPW = %s"
+    cursor.execute(query,(userid,userpw,))
+
+    #fetchall 은 결과를 리스트로 반환 하여 반복하면서 각 행을 출력 하는 용도 
+    result = cursor.fetchall()
+
+    if len(result) == 0:
+        print("아이디 또는 비밀번호가 틀렸습니다.")
+    else:
+        print("로그인 성공 하셨습니다")
+    cursor.close()
+    connection.close()
+
+# 사용자가 접속 하면 사용자 ID 를 넣어서 조회 하고 결과를 변수에 담아서 반환 한다. 
+#UserLogin("tngks7878","ahemsdl00*")
+
+#아이디 찾기 
+
+def UserIDSearch(Userphonenumber):
+    
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user ="root",
+        password ="ahemsdl00*",
+        database ="BingoUserDB"
+    )
+    cursor = connection.cursor(dictionary=True)
+
+    # query 변수에 담아서 SQL 인잭션 방지 하여서 파라미터 바인딩 
+    query = "select * from UserInformation where PhoneNumber = %s"
+    cursor.execute(query,(Userphonenumber,))
+
+    result = cursor.fetchall()
+
+    if len(result) == 0:
+        print("가입된 회원 정보가 없습니다. 회원가입 해주세요")
+    else:
+        for x in result:
+            print(x['UserID'])
+
+#UserIDSearch("010-3530-3370")
+
+def UserSearchPW(UserID):
+    
     connection = mysql.connector.connect(
         host = "localhost",
         user ="root",
@@ -12,19 +68,33 @@ def UserLogin(userid):
 
     # query 변수에 담아서 SQL 인잭션 방지 하여서 파라미터 바인딩 
     query = "select * from UserInformation where UserID = %s"
-    cursor.execute(query,(userid,))
+    cursor.execute(query,(UserID,))
 
-    #fetchall 은 결과를 리스트로 반환 하여 반복하면서 각 행을 출력 하는 용도 
     result = cursor.fetchall()
-    for row in result:
-        print(row)
-    cursor.close()
-    connection.close()
 
-# 사용자가 접속 하면 사용자 ID 를 넣어서 조회 하고 결과를 변수에 담아서 반환 한다. 
-UserLogin("tngks7878")
-
+    if len(result) == 0:
+        print("해당 아이디의 비밀번호를 찾을 수가 없습니다.")
+    else:
+        for x in result:
+            print(x['UserPW'])
 
 
+#회원가입 로직 
+def UserJoin(UserID,UserPW,Name,Phonenumber):
+ 
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user ="root",
+        password ="ahemsdl00*",
+        database ="BingoUserDB"
+    )
+    cursor = connection.cursor(dictionary=True)
+    UserPID = 0
+    query = "select * from UserInformation where idUserInformation = %s"
+    cursor.execute(UserPID)
 
+    # query 변수에 담아서 SQL 인잭션 방지 하여서 파라미터 바인딩 
+    query = "insert into UserInformation (UserID,UserPW,Name,PhoneNumber) value (%s,%s,%s,%s)"
+    cursor.execute(query,(UserID,UserPW,Name,Phonenumber,))
+    
 
